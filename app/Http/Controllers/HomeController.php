e7a567e9bfcf338766be5ecf63f782ac649609ff
Andreas Asatera Sandi Nofa <andreas.nofa@altern8.sg>
2023-08-13 22:19:58 +0700

    feat: add account threshold reached notification in the home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\AccountThresholdReachedEvent;
 use App\Http\Integrations\Idcloudhost\Requests\GetBillingAccountList;
 use App\Settings\GeneralSettings;
 
@@ -13,6 +14,10 @@ class HomeController extends Controller
         $response = $request->send();
 
         $account_lists = $response->collect();
+
+        // account balance reached threshold
+        $account_under_threshold = $account_lists->filter(fn($account) => $account['precalc_ongoing'] < $generalSettings->balance_threshold);
+        AccountThresholdReachedEvent::dispatch($account_under_threshold->toArray());
 
         $data = [
             'account_lists' => $account_lists,
