c115555577b86029d5556c418c373baa34488b94
Andreas Asatera Sandi Nofa <andreas.nofa@altern8.sg>
2023-08-13 10:24:50 +0700

    feat: init native php

---
+++ app/Providers/NativeAppServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Providers;
+
+use Native\Laravel\Facades\MenuBar;
+use Native\Laravel\Contracts\ProvidesPhpIni;
+
+class NativeAppServiceProvider implements ProvidesPhpIni
+{
+    /**
+     * Executed once the native application has been booted.
+     * Use this method to open windows, register global shortcuts, etc.
+     */
+    public function boot(): void
+    {
+        MenuBar::create();
+    }
+
+    /**
+     * Return an array of php.ini directives to be set.
+     */
+    public function phpIni(): array
+    {
+        return [
+        ];
+    }
+}
