a3f99ed4526918ebdba27fd5bb659c37ffcfb30e
Andreas Asatera Sandi Nofa <andreas.nofa@altern8.sg>
2023-08-17 15:13:57 +0700

    test: add test case to ensure accessing home page with low balance will trigger event

--- tests/Feature/HomeTest.php
+++ tests/Feature/HomeTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Events\AccountThresholdReachedEvent;
 use App\Http\Integrations\Idcloudhost\Requests\GetBillingAccountList;
 use App\Livewire\BillingChecker;
 use App\Settings\GeneralSettings;
@@ -20,7 +21,6 @@ test('can access home page with setup api key', function (GeneralSettings $gener
 
     $response = get(route('home'));
 
-    $response->assertOk();
     $response->assertViewIs('home');
     $response->assertSeeText('Billing Account List');
     $response->assertSeeText('Below is the list of billing accounts.');
@@ -29,3 +29,23 @@ test('can access home page with setup api key', function (GeneralSettings $gener
     livewire(BillingChecker::class)
         ->assertViewHas('account_lists', $api_response);
 })->with('api_key_setting', 'api_response');
+
+test('access home page with low balance will trigger event', function (GeneralSettings $generalSettings, array $api_response) {
+    \Saloon\Laravel\Facades\Saloon::fake([
+        GetBillingAccountList::class => MockResponse::make($api_response)
+    ]);
+
+    $generalSettings->balance_threshold = 100000;
+    $generalSettings->save();
+
+    Event::fake();
+    $response = get(route('home'));
+    $response->assertViewIs('home');
+    $response->assertSeeText('Billing Account List');
+    $response->assertSeeText('Below is the list of billing accounts.');
+    $response->assertSeeLivewire(BillingChecker::class);
+
+    livewire(BillingChecker::class)
+        ->assertViewHas('account_lists', $api_response);
+    Event::assertDispatched(AccountThresholdReachedEvent::class);
+})->with('api_key_setting', 'api_response_low_balance');
