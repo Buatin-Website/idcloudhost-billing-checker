2a90a5b3c319c188a0124eb9ff0914b29f106d32
Andreas Asatera Sandi Nofa <andreas.nofa@altern8.sg>
2023-08-17 15:10:39 +0700

    refactor: use class name instead of command name to avoid typo

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\FetchBillingAccountsCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -12,7 +13,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('fetch:billing-accounts')->hourly();
+        $schedule->command(FetchBillingAccountsCommand::class)->hourly();
     }
 
     /**
