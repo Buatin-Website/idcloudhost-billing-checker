a081cd1b093a8b9c761d98b533d19221de7896c3
Andreas Asatera Sandi Nofa <andreas.nofa@altern8.sg>
2023-08-17 15:21:08 +0700

    test: add test cases for configuration page

--- tests/Datasets/SettingsDataset.php
+++ tests/Datasets/SettingsDataset.php
@@ -2,6 +2,17 @@
 
 use App\Settings\GeneralSettings;
 
+dataset('general_settings', fn() => [
+    yield function () {
+        $setting = new GeneralSettings();
+        $setting->api_key = fake()->sha256;
+        $setting->balance_threshold = 100000;
+        $setting->save();
+
+        return $setting;
+    }
+]);
+
 dataset('api_key_setting', fn() => [
     yield function () {
         $setting = new GeneralSettings();
